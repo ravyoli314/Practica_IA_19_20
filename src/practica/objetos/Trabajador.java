180f82f63fb100a0400a9c13a2e71b43f0fb64b0
ravyoli314 <ravyoli314@gmail.com>
2020-05-09 12:41:10 +0200

    extraido tiempo de trabajador

--- src/practica/objetos/Trabajador.java
+++ src/practica/objetos/Trabajador.java
@@ -18,6 +18,7 @@ public class Trabajador {
 	Herramienta herramienta; // herramienta que tiene en mano
 	String area;
 	int tiempoTotalTrabajado;
+	Tiempo tiempo;
 	// A�ADIR LAS VARIABLES NECESARIAS
 
 	/**
@@ -33,8 +34,7 @@ public class Trabajador {
 		this.herramienta = null; // en la primera iteración, el trabajador NO tiene u objeto herramienta (==null)
 		this.area = "A";
 		this.tiempoTotalTrabajado = 0;
-		// A�adir el estado inicial (est�tico) de las variables que se a�adan
-		// Si se necesita a�adir valores variables, como un ID, utilizar setters
+		this.tiempo = new Tiempo();
 	}
 
 	/**
@@ -79,7 +79,7 @@ public class Trabajador {
 		return this.area;
 	}
 	public void setArea(String areaTarea) {
-		int tiempoDesplazamiento = calcularTiempoTrayecto(this.area, areaTarea); 
+		int tiempoDesplazamiento = tiempo.calcularTiempoTrayecto(this.area, areaTarea); 
 		this.tiempoOcupado += tiempoDesplazamiento;
 		this.tiempoTotalTrabajado += tiempoDesplazamiento;
 		this.area = areaTarea;
@@ -125,58 +125,6 @@ public class Trabajador {
 	public void cogerHerramienta(Herramienta herramientaNueva) {
 		setArea("A"); // se encarga de añadirle el tiempo de desplazamiento (0 si ya esta en el almacen)
 		this.herramienta = herramientaNueva;
-	}
-
-	public int calcularTiempoTrayecto(String origen, String destino) {
-		int tiempo = 0;
-
-		if(origen.equals(destino)) return 0;
-		if(origen.equals("A")) {
-			if(destino.equals("J3") || destino.equals("C2") || destino.equals("J2")) tiempo = 5;
-			else if (destino.equals("R") || destino.equals("C1") || destino.equals("J1") || destino.equals("U")) tiempo = 10;
-			else if(destino.equals("B")) tiempo = 15;
-		} else if(origen.equals("R")) {
-			if(destino.equals("J3")) tiempo = 5;
-			else if(destino.equals("A")) tiempo = 10;
-			else if(destino.equals("C2") || destino.equals("J2")) tiempo = 15;
-			else if(destino.equals("C1") || destino.equals("J1") || destino.equals("U")) tiempo = 20;
-			else if(destino.equals("B")) tiempo = 25;
-		} else if(origen.equals("J3")) {
-			if(destino.equals("R") || destino.equals("A")) tiempo = 5; 
-			else if(destino.equals("C2") || destino.equals("J2")) tiempo = 10;
-			else if(destino.equals("C1") || destino.equals("J1") || destino.equals("U")) tiempo = 15;
-			else if(destino.equals("B")) tiempo = 20;
-		} else if(origen.equals("C2")) {
-			if(destino.equals("A") || destino.equals("J1") || destino.equals("C1"))tiempo = 5;
-			else if(destino.equals("J3") || destino.equals("J1") || destino.equals("U")) tiempo = 10;
-			else if(destino.equals("R") || destino.equals("B")) tiempo = 15;
-		} else if(origen.equals("C1")) {
-			if(destino.equals("C2") || destino.equals("J2") || destino.equals("J1") || destino.equals("U")) tiempo = 5;
-			else if(destino.equals("A") || destino.equals("U") || destino.equals("B")) tiempo = 10;
-			else if(destino.equals("J3"))  tiempo = 15;
-			else if(destino.equals("R")) tiempo = 20;
-		} else if(origen.equals("J2")) {
-			if(destino.equals("A") || destino.equals("C2") || destino.equals("C1") || destino.equals("J1") || destino.equals("U")) tiempo = 5;
-			else if(destino.equals("B") || destino.equals("J3")) tiempo = 10;
-			else if(destino.equals("R")) tiempo = 15;
-		} else if(origen.equals("J1")) {
-			if(destino.equals("J2") || destino.equals("C1") || destino.equals("B")) tiempo = 5;
-			else if(destino.equals("A") || destino.equals("C2") || destino.equals("U")) tiempo = 10;
-			else if(destino.equals("J3")) tiempo = 15;
-			else if(destino.equals("R")) tiempo =20;
-		} else if(origen.equals("U")) {
-			if(destino.equals("J2") || destino.equals("B")) tiempo = 5;
-			else if(destino.equals("J1") || destino.equals("C1") || destino.equals("C2") || destino.equals("A")) tiempo = 10;
-			else if(destino.equals("J3")) tiempo = 15;
-			else if(destino.equals("R")) tiempo = 20;
-		}  else if(origen.equals("B")) {
-			if(destino.equals("J1") || destino.equals("U")) tiempo = 5;
-			else if(destino.equals("C1") || destino.equals("J2")) tiempo = 10;
-			else if(destino.equals("C2") || destino.equals("A")) tiempo = 15;
-			else if(destino.equals("J3")) tiempo = 20;
-			else if (destino.equals("J3")) tiempo = 20;
-		}
-		return tiempo;
 	}
 
 
@@ -229,7 +177,7 @@ public class Trabajador {
 	}
 	
 	public void setArea(String areaTarea, double peso) { // Tengo en cuenta el PESO de la herramienta
-		int tiempoDesplazamiento = (int) (calcularTiempoTrayecto(this.area, areaTarea) + (calcularTiempoTrayecto(this.area, areaTarea)/5) * peso);
+		int tiempoDesplazamiento = (int) (tiempo.calcularTiempoTrayecto(this.area, areaTarea) + (tiempo.calcularTiempoTrayecto(this.area, areaTarea)/5) * peso);
 		this.tiempoOcupado += tiempoDesplazamiento; 
 		this.tiempoTotalTrabajado += tiempoDesplazamiento;
 		System.out.println(this.nombre + " tiempo trayecto: " + tiempoDesplazamiento);
